6683cc242e855f334343a5db33264fa3ec7eb7ed
Umesh G <umesh.goslar@philips.com>
2020-10-17 00:34:45 +0530

    latest code

--- app1/src/App.js
+++ app1/src/App.js
@@ -11,34 +11,34 @@ const App = props => {
   };
   return (
     <>
-      <div class="ui small menu">
-        <a class="active item">Home</a>
-        <a class="item">Admin</a>
-        <div class="right menu">
-          <i class="large icons" style={{ marginRight: "2rem" }} onClick={() => buttonClick()}>
+      <div className="ui small menu">
+        <a className="active item">Home</a>
+        <a className="item">Admin</a>
+        <div className="right menu">
+          <i className="large icons" style={{ marginRight: "2rem" }} onClick={() => buttonClick()}>
             <br />
-            <i aria-hidden="true" class="user icon"></i>
+            <i aria-hidden="true" className="user icon"></i>
           </i>
 
-          <div role="listbox" aria-expanded="false" class="ui item dropdown" tabindex="0">
-            <div aria-atomic="true" aria-live="polite" role="alert" class="divider text">Language</div>
-            <i aria-hidden="true" class="dropdown icon"></i>
-            <div class="menu transition">
-              <div role="option" class="item">English</div>
-              <div role="option" class="item">Russian</div>
-              <div role="option" class="item">Spanish</div>
+          <div role="listbox" aria-expanded="false" className="ui item dropdown" >
+            <div aria-atomic="true" aria-live="polite" role="alert" className="divider text">Language</div>
+            <i aria-hidden="true" className="dropdown icon"></i>
+            <div className="menu transition">
+              <div role="option" className="item">English</div>
+              <div role="option" className="item">Russian</div>
+              <div role="option" className="item">Spanish</div>
             </div>
           </div>
-          <div class="item"><button class="ui primary button">Logout</button></div>
+          <div className="item"><button className="ui primary button">Logout</button></div>
         </div>
       </div>
       <div style={{ padding: '50px 12px', display: 'flex' }}>
 
         <br />
-        <div class="ui celled grid">
+        <div className="ui celled grid">
 
-          <div><h1 class="ui header ">Rocc App</h1></div>
-          <div class="row"><div class="three wide column"><img src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image" /></div><div class="thirteen wide column"><img src="https://react.semantic-ui.com/images/wireframe/centered-paragraph.png" class="ui image" /></div></div><div class="row"><div class="three wide column"><img src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image" /></div><div class="ten wide column"><img src="https://react.semantic-ui.com/images/wireframe/paragraph.png" class="ui image" /></div><div class="three wide column"><img src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image" /></div></div></div>
+          <div><h1 className="ui header ">Rocc App</h1></div>
+          <div className="row"><div className="three wide column"><img src="https://react.semantic-ui.com/images/wireframe/image.png" className="ui image" /></div><div className="thirteen wide column"><img src="https://react.semantic-ui.com/images/wireframe/centered-paragraph.png" className="ui image" /></div></div><div className="row"><div className="three wide column"><img src="https://react.semantic-ui.com/images/wireframe/image.png" className="ui image" /></div><div className="ten wide column"><img src="https://react.semantic-ui.com/images/wireframe/paragraph.png" className="ui image" /></div><div className="three wide column"><img src="https://react.semantic-ui.com/images/wireframe/image.png" className="ui image" /></div></div></div>
 
       </div>
     </>
--- app2/src/App.js
+++ app2/src/App.js
@@ -13,103 +13,103 @@ const App = props => {
   };
   return (
     <>
-      <div class="ui small menu">
-        <a class="active item" onClick={() => buttonClick()}>Home</a>
-        <a class="item">Admin</a>
-        <div class="right menu">
-          <i class="large icons" style={{ marginRight: "2rem" }} >
+      <div className="ui small menu">
+        <a className="active item" onClick={() => buttonClick()}>Home</a>
+        <a className="item">Admin</a>
+        <div className="right menu">
+          <i className="large icons" style={{ marginRight: "2rem" }} >
             <br />
-            <i aria-hidden="true" class="user icon"></i>
+            <i aria-hidden="true" className="user icon"></i>
           </i>
 
-          <div role="listbox" aria-expanded="false" class="ui item dropdown" tabindex="0">
-            <div aria-atomic="true" aria-live="polite" role="alert" class="divider text">Language</div>
-            <i aria-hidden="true" class="dropdown icon"></i>
-            <div class="menu transition">
-              <div role="option" class="item">English</div>
-              <div role="option" class="item">Russian</div>
-              <div role="option" class="item">Spanish</div>
+          <div role="listbox" aria-expanded="false" className="ui item dropdown">
+            <div aria-atomic="true" aria-live="polite" role="alert" className="divider text">Language</div>
+            <i aria-hidden="true" className="dropdown icon"></i>
+            <div className="menu transition">
+              <div role="option" className="item">English</div>
+              <div role="option" className="item">Russian</div>
+              <div role="option" className="item">Spanish</div>
             </div>
           </div>
-          <div class="item"><button class="ui primary button">Logout</button></div>
+          <div className="item"><button className="ui primary button">Logout</button></div>
         </div>
       </div>
 
 
       <div className='container'>
         <div className='column'>
-          <div><h1 class="ui header ">Service App</h1></div>
+          <div><h1 className="ui header ">Service App</h1></div>
           <br />
-          <div class="ui four cards">
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Report an issue</div>
-                <div class="description">Leverage agile frameworks to
+          <div className="ui four cards">
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Report an issue</div>
+                <div className="description">Leverage agile frameworks to
                 provide a robust synopsis for high level overviews.
                   </div>
               </div>
             </div>
 
-            <div class="ui card">
-              <div class="content">
-                <div class="header">FAQs</div>
-                <div class="description">Leverage agile frameworks to
-                provide a robust synopsis for high level overviews.
-                  </div>
-              </div>
-            </div>
-
-
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Share an idea</div>
-                <div class="description">Leverage agile frameworks to
-                provide a robust synopsis for high level overviews.
-                  </div>
-              </div>
-            </div>
-
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Learn about ROCC</div>
-                <div class="description">Leverage agile frameworks to
-                provide a robust synopsis for high level overviews.
-                  </div>
-              </div>
-            </div>
-
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Clinical education</div>
-                <div class="description">Leverage agile frameworks to
+            <div className="ui card">
+              <div className="content">
+                <div className="header">FAQs</div>
+                <div className="description">Leverage agile frameworks to
                 provide a robust synopsis for high level overviews.
                   </div>
               </div>
             </div>
 
 
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Subscriptions</div>
-                <div class="description">Leverage agile frameworks to
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Share an idea</div>
+                <div className="description">Leverage agile frameworks to
                 provide a robust synopsis for high level overviews.
                   </div>
               </div>
             </div>
 
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Survey</div>
-                <div class="description">Leverage agile frameworks to
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Learn about ROCC</div>
+                <div className="description">Leverage agile frameworks to
                 provide a robust synopsis for high level overviews.
                   </div>
               </div>
             </div>
 
-            <div class="ui card">
-              <div class="content">
-                <div class="header">Services</div>
-                <div class="description">Leverage agile frameworks to
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Clinical education</div>
+                <div className="description">Leverage agile frameworks to
+                provide a robust synopsis for high level overviews.
+                  </div>
+              </div>
+            </div>
+
+
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Subscriptions</div>
+                <div className="description">Leverage agile frameworks to
+                provide a robust synopsis for high level overviews.
+                  </div>
+              </div>
+            </div>
+
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Survey</div>
+                <div className="description">Leverage agile frameworks to
+                provide a robust synopsis for high level overviews.
+                  </div>
+              </div>
+            </div>
+
+            <div className="ui card">
+              <div className="content">
+                <div className="header">Services</div>
+                <div className="description">Leverage agile frameworks to
                 provide a robust synopsis for high level overviews.
                   </div>
               </div>
